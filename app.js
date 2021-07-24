ee58e3a3ce34b9ea37313f95321135f9d4b0ebda
mustafa-mahmud <mithuweb000@gmail.com>
2021-07-24 19:53:07 +0600

    end project

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 'use strict';
 
 const loader = document.querySelector('.loader');
+const postsContainer = document.querySelector('#posts-container');
+const filter = document.getElementById('filter');
 
 const limit = 5;
-const page = 1;
+let page = 1;
 
 const fetchData = async () => {
   const res = await fetch(
@@ -15,20 +17,55 @@ const fetchData = async () => {
   displayUI(datas);
 };
 
-const displayUI = (datas) => {};
+const displayUI = (datas) => {
+  page++;
+
+  datas.forEach((data) => {
+    const div = document.createElement('div');
+    div.classList.add('post');
+    div.innerHTML = `
+		<div class="number">${data.id}</div>
+		<div class="post-info">
+			<h2 class="post-title">${data.title}</h2>
+			<p class="post-body">${data.body}</p>
+		</div>
+		`;
+
+    postsContainer.appendChild(div);
+  });
+
+  window.addEventListener('scroll', scrollEffect);
+};
+
+const scrollEffect = () => {
+  const bodyHeight = document.body.scrollHeight - 10;
+  const windowScroll = window.innerHeight + window.scrollY;
+
+  if (windowScroll > bodyHeight) {
+    window.removeEventListener('scroll', scrollEffect);
+    loader.classList.add('show');
+    setTimeout(() => {
+      loader.classList.remove('show');
+      fetchData();
+    }, 3000);
+  }
+};
+
+const filterData = (e) => {
+  const titles = postsContainer.querySelectorAll('.post-title');
+  const value = filter.value;
+
+  titles.forEach((title) => {
+    if (title.textContent.trim().includes(value.trim())) {
+      title.closest('.post').style.display = 'flex';
+    } else {
+      title.closest('.post').style.display = 'none';
+    }
+  });
+};
 
 ///////////////////////////
 fetchData();
 
-window.addEventListener('scroll', function () {
-  const bodyHeight = document.body.clientHeight - 50;
-  const windowScroll = window.innerHeight + window.scrollY;
-
-  if (windowScroll > bodyHeight) {
-    loader.classList.add('show');
-
-    /* setTimeout(() => {
-			loader.classList.remove('show');
-		}, 500); */
-  } else loader.classList.remove('show');
-});
+window.addEventListener('scroll', scrollEffect);
+filter.addEventListener('input', filterData);
